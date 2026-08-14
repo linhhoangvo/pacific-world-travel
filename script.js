ec82a6a2d5f53dffb8d6c673c4eb9bbcf0218ad8
linhhoangvo <vothihoanglinh1205@gmail.com>
2026-08-14 15:21:38 +0700

    Update Business Travel and Golf Travel card photos

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ nav?.querySelectorAll("a").forEach((link) => {
     }
   });
 });
+
+const businessTravelPhoto = document.querySelector(".business-feature .feature-photo");
+if (businessTravelPhoto) {
+  businessTravelPhoto.style.backgroundImage = 'url("business-travel.jpg")';
+  businessTravelPhoto.style.backgroundPosition = "center 35%";
+  businessTravelPhoto.style.backgroundSize = "cover";
+  businessTravelPhoto.style.backgroundRepeat = "no-repeat";
+}
+
+const golfTravelPhoto = document.querySelector(".golf-feature .feature-photo");
+if (golfTravelPhoto) {
+  golfTravelPhoto.style.backgroundImage = 'url("golf-travel.jpg")';
+  golfTravelPhoto.style.backgroundPosition = "center 55%";
+  golfTravelPhoto.style.backgroundSize = "cover";
+  golfTravelPhoto.style.backgroundRepeat = "no-repeat";
+}
