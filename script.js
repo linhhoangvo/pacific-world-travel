f2cc55ea2d72daf3354f4d676bbb7201813828ef
linhhoangvo <vothihoanglinh1205@gmail.com>
2026-09-21 16:09:21 +0700

    Remove Who We Are from About dropdown

--- script.js
+++ script.js
@@ -82,7 +82,6 @@ const aboutLink = nav ? [...nav.children].find((item) =>
 ) : null;
 
 buildNavDropdown(aboutLink, "About", [
-  { label: "Who We Are", href: "who-we-are.html" },
   { label: "Our Philosophy", href: "our-philosophy.html" },
   { label: "Why Us", href: "why-us.html" },
   { label: "Our Team", href: "team.html" },
